af10be72b1c9012a59343b6d05cb5bb1969b4ce9
mellsh <mellsh0813@gmail.com>
2025-08-23 09:51:59 +0900

    kotlin study

---
+++ study.kt
@@ -0,0 +1,83 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+
+// fun main() {
+
+// //변수
+// //var = 변경 가능한 변수 선언
+// //val = 변경 불가능한 변수 선언
+//     val a = 3
+//     var b = 2
+
+
+
+// //조건문  
+//     when(a) {
+//         0-> {
+//             println("0!")
+//         }
+//         1-> {
+//             println("1!")
+//         }
+//         2-> {
+//             println("2!")
+//         }
+//         3-> {
+//             println("3!")
+//         }
+//         else -> {
+//             println("else")
+//         }
+//     }
+ 
+   
+// //반복문(while)
+//     var c = 10
+//     while (c>0){
+//         println(c)
+//         c-=1
+//     }
+
+    
+// // 반복문(for)
+// for(num in 1..10) {
+//     print("$num")
+// }
+
+    
+// // 함수
+// println(3+5)
+// println(plus(3,5))
+// }
+// fun plus(a:Int,b:Int):Int {
+//     return a+b
+// }
+
+    
+// // 배열
+// val number1:IntArray = intArrayOf(1,2,3,4,5)    
+// val number2 = intArrayOf(1,2,3,4,5)
+// val number3 = arrayOf(1,2,3,4,5)
+
+// println(number1)
+// println(number1.contentToString())
+    
+// // 이뮤터블 리스트
+// val month = listOf("1월","2월","3월")
+// println(month)
+
+    
+// // 뮤타블 리스트
+// val month = mutableListOf("1월","2월","3월")
+// println(month)
+
+// month.add("4월")
+// println(month)
+
+// month.removeAt(0)
+// println(month)
+}    
+    
+    
